0447cb643318c56009158f63eed4473fc9365fbc
Luthfi Restu Kusumastuti <18120074@mahasiswa.itb.ac.id>
2022-07-30 09:55:00 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,10 @@
-import React, { Profiler } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Homepage from './Component/Begin/Homepage'
-import Customer from './Component/Begin/Customer';
-import Welcome from './Component/Begin/Welcome';
-import Login from './Component/Begin/Login';
-import Signup from './Component/Begin/Signup';
+import Routers from './Component/Routes/Routers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-/*  <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Welcome/>} />
-          <Route path="login" element={<Login/>} />
-          <Route path="signUp" element={<Signup/>} />
-          <Route path="homepage" element={<Homepage/>}/>
-          <Route path="profile" element={<Customer/>} />
-      </Routes>
-</BrowserRouter> */
   <React.StrictMode>
-    <Customer/>
+    <Routers/>
   </React.StrictMode>
 );
