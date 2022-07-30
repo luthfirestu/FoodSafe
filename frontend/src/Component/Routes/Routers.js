5a1e374e98d0c67f6715d833bcb49b278acaeada
Luthfi Restu Kusumastuti <18120074@mahasiswa.itb.ac.id>
2022-07-30 09:53:37 +0700

    Update Routers.js

--- frontend/src/Component/Routes/Routers.js
+++ frontend/src/Component/Routes/Routers.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import {BrowserRouter} from "react-router-dom"
 import { Route, Routes} from "react-router";
-import Homepage from '../App/Homepage'
-import Customer from '../App/Customer'
+import Homepage from '../Begin/Homepage';
+import Customer from '../Begin/Customer';
 import Welcome from '../Begin/Welcome'
 import Login from '../Begin/Login';
 import Signup from '../Begin/Signup';
+import Pricecalc from '../Begin/Pricecalc';
+import Merch from '../Begin/Merch';
+import Home from '../Begin/Home';
+import Profile from '../Begin/Profile';
 
 const Routers = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route exact path='/' element={<Welcome/>} />
-        </Routes>
-        <Routes>
-            <Route path='Home' element={<Homepage/>} />
-        </Routes>
-        <Routes>
-            <Route path='Profile' element={<Customer/>} />
-        </Routes>
-        <Routes>
-            <Route path='Login' element={<Login/>} />
-        </Routes>
-        <Routes>
-            <Route path='SignUp' element={<Signup/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signUp' element={<Signup/>} />
+            <Route path='/pricecalc' element={<Pricecalc/>} />
+            <Route path='/profile' element={<Profile/>} />
+            <Route path='/merch' element={<Merch/>} />
+            <Route path='/home' element={<Home/>} />
         </Routes>
     </BrowserRouter>
   )
